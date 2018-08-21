344b5c9f786185bb94c5f6029d26d517d5a9adcc
Alexander A <gdocse@gmail.com>
2018-08-21 22:45:18 +0300

    Binding a simple C func to Golang

---
+++ CBinding/bind_c.go
@@ -0,0 +1,18 @@
+package main
+
+// #include <stdio.h>
+// #include <errno.h>
+// int f() {
+//  int v[3] = {3, 2 ,5 };
+// 	for (int i = 0; i <3; i++) {
+// 		printf("%d \n", v[i]);
+// 	}
+//  return v[2];
+// }
+import "C"
+import "fmt"
+
+func main() {
+	v := C.f()
+	fmt.Println(v)
+}
