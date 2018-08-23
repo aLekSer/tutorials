bad08cf49d79c60c9a589e162d5551673ff6265c
Alexander A <gdocse@gmail.com>
2018-08-23 16:53:33 +0300

    Cpp code invoke

--- CBinding/bind_c.go
+++ CBinding/bind_c.go
@@ -2,6 +2,7 @@ package main
 
 // #include <stdio.h>
 // #include <errno.h>
+// #include "sort.h"
 // int f() {
 //  int v[3] = {3, 2 ,5 };
 // 	for (int i = 0; i <3; i++) {
@@ -15,4 +16,5 @@ import "fmt"
 func main() {
 	v := C.f()
 	fmt.Println(v)
+	C.cpp()
 }
